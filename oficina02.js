6fa31456ab79603baec7528d08166987c427219f
Giovanna Lara da Silva Freitas <126110106+gigilara02@users.noreply.github.com>
2023-03-10 15:08:28 -0300

    Add files via upload

---
+++ oficina02.js
@@ -0,0 +1,15 @@
+let temsol = true;
+
+if (temdinheiro) {
+   if (temsol) {
+     console.log("guaruja");
+   } else {
+    console.log("restaurante");
+   }   
+} else {  
+   if(temsol) {
+    console.log("ibirapuera");
+   } else {
+    console.log("aula");
+   }
+}
